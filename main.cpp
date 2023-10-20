d6754f57488a4c0fdcef11ba04765601c2a005ee
matmuher <barbusas.mail@gmail.com>
2023-10-20 20:45:39 +0300

    [matmuh] review client.hpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@
 #include "bus.hpp"
 #include "bike.hpp"
 #include "client.hpp"
-
+// try add #include "client.hpp" again
+// it happens often in big projects,
+// where one modulo reference another one two three...
 
 
 int main()
